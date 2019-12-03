652d8a2ed95a555d26d7979940512684dc3ed5c5
Pave Aljinović <paljinovic@gmail.com>
2019-12-03 19:12:42 +0100

    Optimise year 2017 - day2 puzzle

--- src/Puzzle/Year2017/Day2CorruptionChecksum/Part1.php
+++ src/Puzzle/Year2017/Day2CorruptionChecksum/Part1.php
@@ -1,22 +1,18 @@
 <?php
 
-namespace App\Puzzle\Year2017\Day2CorruptionChecksum;
-
-use App\Puzzle\PuzzleInterface;
-
 /*
 --- Day 2: Corruption Checksum ---
 
-As you walk through the door, a glowing humanoid shape yells in your
-direction. "You there! Your state appears to be idle. Come help us repair
-the corruption in this spreadsheet - if we take another millisecond, we'll
-have to display an hourglass cursor!"
+As you walk through the door, a glowing humanoid shape yells in your direction.
+"You there! Your state appears to be idle. Come help us repair the corruption in
+this spreadsheet - if we take another millisecond, we'll have to display an
+hourglass cursor!"
 
-The spreadsheet consists of rows of apparently-random numbers. To make sure
-the recovery process is on the right track, they need you to calculate the
+The spreadsheet consists of rows of apparently-random numbers. To make sure the
+recovery process is on the right track, they need you to calculate the
 spreadsheet's checksum. For each row, determine the difference between the
-largest value and the smallest value; the checksum is the sum of all of
-these differences.
+largest value and the smallest value; the checksum is the sum of all of these
+differences.
 
 For example, given the following spreadsheet:
 
@@ -24,8 +20,8 @@ For example, given the following spreadsheet:
 7 5 3
 2 4 6 8
 
-- The first row's largest and smallest values are 9 and 1, and their
-  difference is 8.
+- The first row's largest and smallest values are 9 and 1, and their difference
+  is 8.
 - The second row's largest and smallest values are 7 and 3, and their
   difference is 4.
 - The third row's difference is 6.
@@ -35,44 +31,54 @@ In this example, the spreadsheet's checksum would be 8 + 4 + 6 = 18.
 What is the checksum for the spreadsheet in your puzzle input?
 */
 
+namespace App\Puzzle\Year2017\Day2CorruptionChecksum;
+
+use App\Puzzle\PuzzleInterface;
+
 class Part1 implements PuzzleInterface
 {
     /**
      * Determines checksum for the spreadsheet.
      *
-     * @return int Checksum
+     * @param string $spreadsheetMultilineString
+     * 
+     * @return integer Checksum
      */
-    function solution(): int
+    public function solution($spreadsheetMultilineString)
     {
         $rowsDiff = [];
 
-        $spreadsheet = getSpreadsheetData();
+        // Spreadsheet data is parsed from multiline string and stored to array
+        $spreadsheet = $this->parseMultilineString($spreadsheetMultilineString);
 
         foreach ($spreadsheet as $row) {
+            // Determine the difference between the largest value and the smallest value
+
             $max = max($row);
             $min = min($row);
 
             $rowsDiff[] = $max - $min;
         }
 
+        //  The checksum is the sum of all row differences
         $checksum = array_sum($rowsDiff);
 
         return $checksum;
     }
 
     /**
-     * Reads spreadsheet text file and fills array with data from file.
+     * Reads spreadsheet multiline string and stores data to array.
      *
+     * @param string $spreadsheetMultilineString
+     * 
      * @return array
      */
-    function getSpreadsheetData(): array
+    public function parseMultilineString(string $spreadsheetMultilineString): array
     {
         $data = [];
 
-        $spreadsheetString = file_get_contents('./spreadsheet.txt');
-
-        $spreadsheetString = str_replace("\t", " ", $spreadsheetString);
-        $rows = preg_split("/(\r\n|\n|\r)/", $spreadsheetString);
+        $spreadsheetMultilineString = str_replace("\t", " ", $spreadsheetMultilineString);
+        $rows = preg_split("/(\r\n|\n|\r)/", $spreadsheetMultilineString);
 
         foreach ($rows as $row) {
             $rowData = explode(" ", $row);
@@ -82,6 +88,3 @@ class Part1 implements PuzzleInterface
         return $data;
     }
 }
-
-$checksum = solution();
-echo $checksum;
--- src/Puzzle/Year2017/Day2CorruptionChecksum/Part2.php
+++ src/Puzzle/Year2017/Day2CorruptionChecksum/Part2.php
@@ -1,26 +1,21 @@
 <?php
 
-namespace App\Puzzle\Year2017\Day2CorruptionChecksum;
-
-use App\Puzzle\PuzzleInterface;
-
-
 /*
 --- Part Two ---
 
-"Great work; looks like we're on the right track after all. Here's a star
-for your effort." However, the program seems a little worried. Can programs
-be worried?
+"Great work; looks like we're on the right track after all. Here's a star for
+your effort." However, the program seems a little worried. Can programs be
+worried?
 
 "Based on what we're seeing, it looks like all the User wanted is some
-information about the evenly divisible values in the spreadsheet.
-Unfortunately, none of us are equipped for that kind of calculation - most
-of us specialize in bitwise operations."
+information about the evenly divisible values in the spreadsheet. Unfortunately,
+none of us are equipped for that kind of calculation - most of us specialize in
+bitwise operations."
 
-It sounds like the goal is to find the only two numbers in each row where
-one evenly divides the other - that is, where the result of the division
-operation is a whole number. They would like you to find those numbers on
-each line, divide them, and add up each line's result.
+It sounds like the goal is to find the only two numbers in each row where one
+evenly divides the other - that is, where the result of the division operation
+is a whole number. They would like you to find those numbers on each line,
+divide them, and add up each line's result.
 
 For example, given the following spreadsheet:
 
@@ -28,8 +23,8 @@ For example, given the following spreadsheet:
 9 4 7 3
 3 8 6 5
 
-- In the first row, the only two numbers that evenly divide are 8 and 2;
-  the result of this division is 4.
+- In the first row, the only two numbers that evenly divide are 8 and 2; the
+  result of this division is 4.
 - In the second row, the two numbers are 9 and 3; the result is 3.
 - In the third row, the result is 2.
 
@@ -38,61 +33,51 @@ In this example, the sum of the results would be 4 + 3 + 2 = 9.
 What is the sum of each row's result in your puzzle input?
 */
 
+namespace App\Puzzle\Year2017\Day2CorruptionChecksum;
+
+use App\Puzzle\PuzzleInterface;
 
 class Part2 implements PuzzleInterface
 {
-/**
- * Determines checksum for the spreadsheet.
- *
- * @return int Result
- */
-function solution(): int
-{
-    $rowsQuotient = [];
+    private $part1;
 
-    $spreadsheet = getSpreadsheetData();
+    public function __construct()
+    {
+        // Needed function(s) are already implemented in part1
+        $this->part1 = new Part1();
+    }
 
-    foreach ($spreadsheet as $row) {
-        rsort($row);
+    /**
+     * Determines checksum for the spreadsheet.
+     *
+     * @param string $spreadsheetMultilineString
+     * 
+     * @return integer Division sum
+     */
+    public function solution($spreadsheetMultilineString)
+    {
+        $rowsQuotient = [];
 
-        // Trying to divide max with min number, there is a greater chance divisor will be found sooner
-        for ($i = 0; $i < count($row) - 1; $i++) {
-            for ($j = count($row) - 1; $j > $i; $j--) {
-                if ($row[$i] % $row[$j] === 0) {
-                    $rowsQuotient[] = $row[$i] / $row[$j];
-                    break 2;
+        // Spreadsheet data is parsed from multiline string and stored to array
+        $spreadsheet = $this->part1->parseMultilineString($spreadsheetMultilineString);
+
+        foreach ($spreadsheet as $row) {
+            rsort($row);
+
+            // Trying to divide max with min number, there is a greater chance divisor will be found sooner
+            for ($i = 0; $i < count($row) - 1; $i++) {
+                for ($j = count($row) - 1; $j > $i; $j--) {
+                    if ($row[$i] % $row[$j] === 0) {
+                        $rowsQuotient[] = $row[$i] / $row[$j];
+                        break 2;
+                    }
                 }
             }
         }
+
+        //  The division sum is the sum of row whole number quotients
+        $divisionSum = array_sum($rowsQuotient);
+
+        return $divisionSum;
     }
-
-    $checksum = array_sum($rowsQuotient);
-
-    return $checksum;
 }
-
-/**
- * Reads spreadsheet text file and fills array with data from file.
- *
- * @return array
- */
-function getSpreadsheetData(): array
-{
-    $data = [];
-
-    $spreadsheetString = file_get_contents('./spreadsheet.txt');
-
-    $spreadsheetString = str_replace("\t", " ", $spreadsheetString);
-    $rows = preg_split("/(\r\n|\n|\r)/", $spreadsheetString);
-
-    foreach ($rows as $row) {
-        $rowData = explode(" ", $row);
-        $data[] = array_map('intval', $rowData);
-    }
-
-    return $data;
-}
-}
-
-$checksum = solution();
-echo $checksum;
